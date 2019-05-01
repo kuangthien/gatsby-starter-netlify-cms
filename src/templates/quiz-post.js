a26a82c2a467e1f9833b2e583178c9bd560eab54
Tran Kuang Thien <kuangthien@gmail.com>
2019-05-01 14:06:09 +0700

    * quizList page with route /quizzes * dummy quiz post content

---
+++ src/templates/quiz-post.js
@@ -0,0 +1,16 @@
+import { graphql } from 'gatsby';
+
+export const pageQuery = graphql`
+	query QuizPostByID($id: String!) {
+		markdownRemark(id: { eq: $id }) {
+			id
+			html
+			frontmatter {
+				date(formatString: "MMMM DD, YYYY")
+				title
+				description
+				tags
+			}
+		}
+	}
+`;
