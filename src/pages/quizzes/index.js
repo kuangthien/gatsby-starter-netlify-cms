a1e85684e6a658a99b97b839621ba408be9e2d73
Tran Kuang Thien <kuangthien@gmail.com>
2019-05-01 14:06:09 +0700

    * quizList page with route /quizzes * dummy quiz post content

---
+++ src/pages/quizzes/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Layout from '../../components/Layout';
+import QuizList from '../../components/QuizList';
+
+const QuizzesIndexPage = () => {
+	return (
+		<Layout>
+			<QuizList />
+		</Layout>
+	);
+};
+
+export default QuizzesIndexPage;
