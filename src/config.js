428e979700bddc790f7632112fca3aac42b2be11
Tran Kuang Thien <kuangthien@gmail.com>
2019-06-02 16:23:01 +0700

    remove uneeded things

--- src/config.js
+++ src/config.js
@@ -1,10 +1,6 @@
 const API_URL = process.env.REACT_APP_API_URL
 
-const STATIC_ASSETS_URL =
-  navigator.userAgent !== 'ReactSnap'
-    ? `${process.env.REACT_APP_ROOT_SITE_URL}contentJson/`
-    : `http://localhost:45678/contentJson/`
-
+const STATIC_ASSETS_URL = ''
 const SITE_TITLE = 'AhaQuiz.com'
 const ROOT_SITE_URL = process.env.REACT_APP_ROOT_SITE_URL
 
